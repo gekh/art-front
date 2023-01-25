752c8b334c975518adab77b132834f10fd2f91f5
German Khokhlov <lkg0dzre@gmail.com>
2023-01-25 06:36:28 +0530

    fixed pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -21,7 +21,7 @@ module.exports = {
       ref: 'origin/main',
       repo: 'git@art-front.github.com:gekh/art-front.git',
       path: '/var/vhosts/art-front/',
-      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.cjs --env production',
     }
   }
 };
