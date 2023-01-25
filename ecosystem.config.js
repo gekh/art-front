166914789408d9e59df16008d6fe726141bd9cdb
German Khokhlov <lkg0dzre@gmail.com>
2023-01-25 05:35:54 +0530

    added pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps: [
+    {
+      name: 'ArtCompass',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: 'build/index.js',
+      env_production: {
+        NODE_ENV: 'production',
+        HOST:"127.0.0.1",
+        PORT:4000,
+        ORIGIN:"http://artcompas.com",
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'art',
+      host: '89.108.78.111',
+      ref: 'origin/main',
+      repo: 'git@github.com:gekh/art-front.git',
+      path: '/var/vhosts/art-front/',
+      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+    }
+  }
+};
