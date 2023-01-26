4d92a0afece6cba384ed4aaf2dc5bd37451b85ff
German Khokhlov <lkg0dzre@gmail.com>
2023-01-26 23:33:11 +0530

    front markup: bg & NavBar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,36 @@
 <script lang="ts">
+	import NavBar from "./NavBar.svelte";
 </script>
 
 <svelte:head>
 	<title>Art Compas</title>
 	<meta content="Art Compass" property="og:title" />
 	<meta content="Art Compass" property="twitter:title" />
-</svelte:head>;
+</svelte:head>
 
-<template lang="pug">
-	div.body
-		h1 Art Compass
-
+<template	lang="pug">
+	.background-fill
+	.background-bubbles
+	NavBar
+	.h-96 hi there
+	.h-96 hi there
+	.h-96 hi there
+	.h-96 hi there
 </template>
 
+
+
 <style lang="postcss">
-	.body {
-		@apply bg-black
+	.background-fill {
+		@apply fixed left-0 right-0 top-0 bottom-0 z-0;
+		background: linear-gradient(-45deg, #001739, #032241);
+	}
+
+	.background-bubbles {
+		@apply fixed left-0 right-0 top-0 bottom-0 z-0 opacity-50;
+		background: url("images/bg-bubbles.png") 50% 50%;
+		/* background-image: url("images/bg-bubbles.png"); */
+		background-size: cover;
+		/* background-position: 50% 50%; */
 	}
 </style>
---
+++ src/routes/NavBar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	function toTop() {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth',
+		});
+	}
+</script>
+
+<template lang="pug">
+  .fixed.left-0.right-0.flex
+    a.inline-block(href="#top" on:click!="{toTop}")
+      img.b-logo.w-8(src="logo/logo.svg" alt="ArtCompas Logo")
+
+    .flex.ml-auto
+      .b-menu.flex.items-center.opacity-90
+        a(href="#top") Главная
+        a(href="#how-to-stat") Как начать
+        a(href="#for-whom") Кому подойдет
+        a(href="#for-cutomer") Для заказчика
+        a(href="#for-artist") Для исполнителя
+        a(href="#benefits") В чем польза
+</template>
+
+<style lang="postcss">
+	.b-logo {
+		margin: 26px;
+	}
+
+	.b-menu a {
+		@apply px-5 font-light flex items-center;
+    height: 84px;
+    font-size: 15px;
+    background: linear-gradient(to top, rgba(100,0,0,0) 50%, #fd012e 50%) bottom;
+    background-size: 100% 200%;
+    transition:all 0.5s ease;
+	}
+
+  .b-menu a:hover {
+    background-position: top;
+  }
+</style>
