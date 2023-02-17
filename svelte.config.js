7ecffd366507427dcf8a15876ca9240579df07a0
German Khokhlov <lkg0dzre@gmail.com>
2023-02-17 21:11:59 +0530

    updated svelte config to fit with tailwind guide

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,12 @@
-import adapter from '@sveltejs/adapter-node';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
+// /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter({
-			precompress: false,
-		})
-	}
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
 };
 
 export default config;
